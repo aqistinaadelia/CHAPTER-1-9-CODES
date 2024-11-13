cb8baed45a7a47a55d97e79e18795fc0f5209e3a
aqistinaadelia <ainaqistina.ahmadhalimi@student.gmi.edu.my>
2024-11-13 17:55:47 +0800

    Add files via upload

---
+++ WhileLoopDemo.java
@@ -0,0 +1,14 @@
+
+package whileloopdemo;
+public class WhileLoopDemo {
+
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println("Printing Numbers from 10 to 0");
+        while (count >= 0) {
+            System.out.println(count--);}
+        }
+       
+    }
+    
+
---
+++ YourFullName.java
@@ -0,0 +1,28 @@
+
+package yourfullname;
+import java.util.Scanner;
+public class YourFullName {
+
+ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input your first name:");
+        String fname = input.nextLine();
+       
+        System.out.print("Input your last name:");
+        String lname = input.nextLine();
+        
+        
+        System.out.println("\nHello\n" +fname +lname);      
+        
+        // Aina Qistina Adelia 
+        
+        
+        
+        
+        
+       
+    }
+    
+}
