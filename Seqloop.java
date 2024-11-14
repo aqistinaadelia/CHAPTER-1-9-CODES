7054d33aeaf47fea8240ab443fa9f98b9ec852bb
aqistinaadelia <ainaqistina.ahmadhalimi@student.gmi.edu.my>
2024-11-14 16:55:44 +0800

    Add files via upload

---
+++ Seqloop.java
@@ -0,0 +1,16 @@
+
+package seqloop;
+public class Seqloop {
+
+    public static void main(String[] args) {
+        
+        int mula = 99;
+        int decrement = 11;
+
+        for (int i = mula; i >= 11; i -= decrement) {
+            System.out.print(i + " ");
+        }
+    }
+}
+
+ 
---
+++ string.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package yourfullname;
+
+/**
+ *
+ * @author User
+ */
+class string {
+
+    static String fname;
+    static String lname;
+    
+}
